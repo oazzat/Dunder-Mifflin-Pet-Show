70198dcb9933e0fc3467cb6d823ede1aa7f68db6
oazzat <oazzat19@gmail.com>
2018-11-29 10:54:44 -0500

    first commit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,45 @@
 class DogsController < ApplicationController
+before_action :find_dog, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    redirect_to @dog
+  end
+
+  def edit
+
+  end
+
+  def update
+    @dog.update(dog_params)
+    redirect_to @dog
+  end
+
+  def destroy
+    @dog.destroy
+    redirect_to dogs_path
+  end
+
+  private
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
 end
